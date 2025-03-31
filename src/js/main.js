d8f7c5b4744d76c760abb30da21a624c2d90f489
elielino360 <appschoolbic@gmail.com>
2025-03-31 19:30:31 +0100

    fixing of alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
+import Alert from "./alert.mjs";
+
+const alertInstance = new Alert("/json/alerts.json");
+alertInstance.render();
+
 loadHeaderFooter();
